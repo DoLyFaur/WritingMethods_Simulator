6db34c51871dcfa4071cbafe61c4cef11bfba2d1
DoLyFaur <faurdorotheealidia@ymail.com>
2021-10-15 12:43:33 +0300

    Beginning
    Add Instruction class

---
+++ WritingMethods_Simulator/Instruction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritingMethods_Simulator
+{
+    class Instruction
+    {
+        public char opcode;
+        int PC_crt;
+        int date_instr;
+    }
+}
--- WritingMethods_Simulator/SmallMemory/InstructionBuffer.cs
+++ WritingMethods_Simulator/SmallMemory/InstructionBuffer.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace WritingMethods_Simulator.SmallMemory
 {
-    class InstructionBuffer
+    class InstructionBuffer:Queue<Instruction>
     {
+        int size;
+        public InstructionBuffer(int size)
+        {
+            this.size = size;
+        }
+
+        public void Read(int quantity, )
     }
 }
